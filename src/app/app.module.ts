f54ff13ad4f2007a348e2f373c61a97f2ef4923f
Chayasaini <chayasaini99@gmail.com>
2020-12-25 19:41:51 +0530

    Intial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,106 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { NewsApiService } from "./news-api.service";
 
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatListModule,
+  MatPaginatorModule,
+  MatOptionModule, 
+  MatCheckboxModule, 
+  MatInputModule,
+  MatAutocompleteModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatSliderModule,
+  MatSlideToggleModule, 
+  MatGridListModule, 
+  MatStepperModule,
+  MatTabsModule,
+  MatExpansionModule,
+  MatButtonToggleModule,
+  MatChipsModule, 
+  MatProgressSpinnerModule,
+  MatProgressBarModule,
+  MatDialogModule,
+  MatTooltipModule,
+  MatSnackBarModule,
+  MatTableModule,
+  MatSortModule
+} from "@angular/material";
+import { RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { LiveNewsComponent } from "./live-news/live-news.component";
+import { HomeComponent } from "./home/home.component";
+
+import { APP_ROUTES } from "./app.routing";
+import { EntertainmentComponent } from "./entertainment/entertainment.component";
+import { HealthComponent } from "./health/health.component";
+import { TechnologyComponent } from "./technology/technology.component";
+import { SportsComponent } from "./sports/sports.component";
+import { NgxYoutubePlayerModule } from "ngx-youtube-player";
+import { SearchNewsComponent } from './search-news/search-news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LiveNewsComponent,
+    HomeComponent,
+    EntertainmentComponent,
+    HealthComponent,
+    TechnologyComponent,
+    SportsComponent,
+    SearchNewsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+  MatCardModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatListModule,
+  MatPaginatorModule,
+  MatOptionModule, 
+  MatCheckboxModule, 
+  MatInputModule,
+  MatAutocompleteModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatSliderModule,
+  MatSlideToggleModule, 
+  MatGridListModule, 
+  MatStepperModule,
+  MatTabsModule,
+  MatExpansionModule,
+  MatButtonToggleModule,
+  MatChipsModule, 
+  MatProgressSpinnerModule,
+  MatProgressBarModule,
+  MatDialogModule,
+  MatTooltipModule,
+  MatSnackBarModule,
+  MatTableModule,
+  MatSortModule,
+    RouterModule.forRoot(APP_ROUTES),
+    NgxYoutubePlayerModule.forRoot()
   ],
-  providers: [],
+  providers: [NewsApiService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+//
